f6af8861d206d4d955395d51f3cbbf7c5278a345
caithomas6789 <caigsthomas@gmail.com>
2024-01-03 12:07:08 +0000

    Added more data cleaning to transform script

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,15 +1,16 @@
+"""Script which runs the full ETL pipeline."""
 from datetime import datetime
 from time import perf_counter
 
 from extract import load_sales_data, extract_data_from_json
-from transform import clean_tags
+from transform import clean_dataframe
 
 if __name__ == "__main__":
     start = perf_counter()
     sales_data = load_sales_data(datetime.now())
     extracted_data = extract_data_from_json(sales_data)
 
-    extracted_data['tags'] = extracted_data['tags'].apply(clean_tags)
-    extracted_data.to_csv("clean_data.csv", index=False)
+    clean_data = clean_dataframe(extracted_data)
+    clean_data.to_csv("clean_data.csv", index=False)
 
     print(f"Time taken: {perf_counter() - start}")
--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,28 +1,59 @@
-import spacy
+"""
+Script to clean and transform all the data from the extract script.
+"""
 import pandas as pd
 
 DNB = ['Drum & Bass', 'Dnb', 'Drum N Bass']
 RNB = ['Rnb', 'R&B']
 
 
-def clean_tags(x):
-    print(x)
-    s = set()
-    for tag in x:
+def clean_tags(tags: list[str]) -> list[str]:
+    """
+    Cleans the tags associated with the album / track.
+    """
+    tags_set = set()
+    for tag in tags:
         if '-' in tag:
             tag = tag.replace('-', ' ')
         if '/' in tag:
             tags = tag.split('/')
             for extra_tag in tags:
-                s.add(extra_tag.title())
+                tags_set.add(extra_tag.title())
             continue
         if tag[-1] == '.':
             tag = tag[:-1]
         if tag.title() in DNB:
-            s.add('DNB')
+            tags_set.add('DNB')
         elif tag.title in RNB:
-            s.add('R&B')
+            tags_set.add('R&B')
         else:
-            s.add(tag.title())
-    print(s)
-    return list(s)
+            tags_set.add(tag.title())
+    return list(tags_set)
+
+
+def clean_artists(name: str) -> str:
+    """
+    For artist names that feature other artists,
+    featured artists are erased and only the main artist is left
+    """
+    artists = name.split("ft.")
+    return artists[0]
+
+
+def clean_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Cleans the tags, the title, the amount paid for the album / track
+    and the artist.
+    """
+    dataframe['tags'] = dataframe['tags'].apply(clean_tags)
+
+    dataframe['title'] = dataframe['title'].str.strip()
+
+    dataframe['amount_paid_usd'] = dataframe['amount_paid_usd'] * 100
+    dataframe['amount_paid_usd'] = dataframe['amount_paid_usd'].astype(int)
+
+    dataframe['artist'] = dataframe['artist'].apply(clean_artists)
+
+    dataframe = dataframe.explode('tags')
+
+    return dataframe
