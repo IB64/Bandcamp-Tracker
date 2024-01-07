7175193b39ba59fd57a3e6f9bb9867c5f7dd669c
Caitlin Turnidge <caitlinturnidge@Caitlins-MacBook-Pro.local>
2024-01-07 08:50:52 +0000

    add home page

---
+++ dashboard/Home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="BandCamp Analytics",
+    page_icon="🎵",
+)
+
+st.write("# BandCamp Analytics")
+
+st.write("#### Welcome to BandCamp Analytics")
